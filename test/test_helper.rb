28a1f7398d9d75057ad35f94a749ec96ef43841c
Andrew Kane <andrew@ankane.org>
2024-02-29 17:50:30 -0800

    Debug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
+
+class Minitest::Test
+  def setup
+    puts "#{self.class.name}##{name}"
+  end
+end
